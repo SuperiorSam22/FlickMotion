aa8fc6360dca434d7f97e667d85cf8833fa48173
Sandeep Lal <sandeep221102@gmail.com>
2023-12-03 18:25:12 +0530

    added switching tabs

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./style.scss";
 
-const SwitchTabs = () => {
-  return (
-    <div>SwitchTabs</div>
-  )
+const SwitchTabs = ({data, onTabChange}) => {
+    const [selectedtab, setSelectedTab] = useState(0);
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, index)=> {
+        setLeft(index * 100)
+        setTimeout(() => {
+            setSelectedTab(index);
+        }, 300);
+        onTabChange(tab, index);
+    }
+
+  return (<div className="switchingTabs">
+    
+    <div className="tabItems">
+        {data.map((tab,index) => (
+            <span 
+                key={index} 
+                className={`tabItem ${selectedtab === index ? "active" : ""}`}
+                onClick={()=> activeTab(tab, index)}
+                >
+                {tab}
+            </span>
+        ))}
+        <span className="movingBg" style={{left}}/>
+    </div>
+  </div> );
 }
 
 export default SwitchTabs;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ContentWrapper from '../../../components/contenetWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
 //we have to call api here too 
 
 
 const Trending = () => {
+    const [endpoint, setEndpoint] = useState("day");
+    const {data, loading} = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab)=> {
+        setEndpoint(tab === "Day" ? "day": "week");
+  }
   return (
     <div className='carouselSection'>
         <ContentWrapper>    
             <span className="carouselTitle">Trending</span> 
-            <SwitchTabs data={["Day", "Week"]} />   
+            <SwitchTabs data={["Day", "Week"]} 
+                onTabChange={onTabChange}/>   
             
             
         </ContentWrapper>    
