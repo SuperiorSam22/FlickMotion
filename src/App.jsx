47baa714978672e433c5e8637023a23d84bdada2
Sandeep Lal <sandeep221102@gmail.com>
2023-12-04 19:51:38 +0530

    added genres

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {fetchDataFromApi} from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration } from './store/homeSlice';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 import Header from "./components/header/header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
@@ -19,7 +19,8 @@ function App() {
     console.log(url);
 
     useEffect(()=> {
-        fetchApiConfig()
+        fetchApiConfig();
+        genresCall();
     }, []);
 
     const fetchApiConfig = () => {
@@ -34,8 +35,26 @@ function App() {
                 }
                 dispatch(getApiConfiguration(url));
             });
+        
     }
 
+    const genresCall = async ()=>{
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`))
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({genres}) => {
+            return genres.map((item) => (allGenres[item.id] = item)) 
+        }); 
+        
+        dispatch(getGenres(allGenres));
+    }
 
     return (<BrowserRouter> 
     <Header/>
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -11,6 +11,7 @@ import ContentWrapper from "../contenetWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 import "./style.scss";
 
 const Carousel = ({data, loading}) => {
@@ -30,6 +31,17 @@ const Carousel = ({data, loading}) => {
         )
     }
     const navigation = (dir) => {
+        const container = carouselContainer.current;
+
+        const scrollAmount = dir === "left" ? 
+        container.scrollLeft - (container.offsetWidth + 20)
+        : 
+        container.scrollLeft + (container.offsetWidth + 20);
+
+        container.scrollTo({
+            left: scrollAmount ,
+            behavior: "smooth",
+        })
 
     };
   return (
@@ -44,16 +56,20 @@ const Carousel = ({data, loading}) => {
                 onClick={()=> navigation("right")}
             />
             {!loading ? (
-                <div className="carouselItems">
+                <div className="carouselItems" ref ={carouselContainer}>
                     {data?.map((item) => {
                         const posterUrl = item.poster_path ? 
                         url.poster + item.poster_path : PosterFallback;
                         return (
                             <div key={item.id}
-                                className="carouselItem">
+                                className="carouselItem" 
+                                    onClick={()=> navigate(`/${item.media_type}/${item.id}`)}>
                                     <div className="posterBlock">
                                         <Img src={posterUrl}/>
-                                        <CircleRating rating={item.vote_average.toFixed(1)}/>                                    </div>
+                                        <CircleRating rating={item.vote_average.toFixed(1)}/>
+
+                                        <Genres data={item.genre_ids.slice(0,2)}/>                                    
+                                    </div>
                                     <div className="textBlock">
                                         <span className="title">
                                             {item.title || item.name}
