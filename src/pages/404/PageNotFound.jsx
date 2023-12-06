38b9720145ff92b14c521417947f2bc62ca2775e
Sandeep Lal <sandeep221102@gmail.com>
2023-12-06 19:49:38 +0530

    pagenotfound updated

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./style.scss";
 
-import ContentWrapper from "../../components/contenetWrapper/ContentWrapper";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
     return (
